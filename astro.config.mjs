0144c9e98380c4e8444fdd9a8e3084885887230e
Blessing Mashile <48390318+Bless12Mash@users.noreply.github.com>
2025-05-25 17:24:05 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://blessingmashilezwane.github.io',
+  base: 'portfolio',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
